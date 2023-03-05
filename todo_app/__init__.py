5778e585c0c914b8339a82624872def2f7652e69
Krzysztof Putra <krz.putra97@gmail.com>
2023-03-06 00:59:49 +0100

    *Added login_manager *Added db User and Todo models

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 SECRET_KEY = "insert secret key"
 app = Flask(__name__)
@@ -8,10 +10,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+from todo_app.api.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/index')
 def home():
     return "Initial File"
 
-with app.app_context():
-    db.create_all()
---
+++ todo_app/api/models.py
@@ -0,0 +1,24 @@
+from todo_app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True)
+    password = db.Column(db.String)
+    todos = db.relationship('Todo', backref='owner')
+
+
+    def __repr__(self):
+        return f'User: {self.first_name} {self.email}'
+    
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    todo_name = db.Column(db.String(255))
+    deadline = db.Column(db.DateTime())
+    status = db.Column(db.String(255))
+    todo_owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'ToDo: {self.todo_name} [{self.deadline}]'
